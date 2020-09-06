fbc59fb556caee36161aa443fcf968f29d08a10e
DheoAp <dheoap21@gmail.com>
2020-09-06 14:19:40 +0700

    Membuat about me

--- application/views/about/header.php
+++ application/views/about/header.php
@@ -4,9 +4,39 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="<?= base_url().'assets/bootstrap/bootstrap.min.css'?>">
+  
   <title>About Me</title>
+  <style>
+    body{
+      background-image: url('assets/vintage-wallpaper.png');
+    }
+    .card{
+      background-color: #dfe4ea;
+      margin: 50px auto;
+    }
+    
+  </style>
 </head>
 <body>
-  <h1>Halaman ini di buat dengan bootstrap</h1>
-</body>
-</html>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="<?= base_url().'musik'?>">Dheo Apriansyah</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+    <div class="navbar-nav">
+      <a class="nav-link active" href="<?= base_url().'about'?>">Home <span class="sr-only">(current)</span></a>
+      <ul class="navbar-nav">
+        <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Portfolio
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+          <a class="dropdown-item" href="<?= base_url().'musik'?>">App Musik</a>
+          <!-- <a class="dropdown-item" href="#">Blog</a> -->
+        </div>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
---
+++ application/views/about/index.php
@@ -0,0 +1,21 @@
+<div class="container mt-5">
+  <div class="card mb-3" style="max-width: 560px;">
+    <div class="row no-gutters">
+      <div class="col-md-4 p-2">
+        <img src="assets/dheo.jpg" class="card-img mt-2" alt="...">
+      </div>
+      <div class="col-md-8">
+        <div class="card-body">
+          <h5 class="card-title">Dheo Apriansyah</h5>
+          <p class="card-text">Sedang mencari pekerjaan di bidang web programming, dimana saya dapat menunjukan, mengembangkan keahlian saya dan akan selalu belajar dan belajar.</p>
+          <hr>
+          <p class="card-text">Universitas Bina Sarana Informatika 2016-2019
+          <br>Lulusan D3 Sistem Infomatika - 3.02/4.00</p>
+          <a target="_blank" href="https://www.linkedin.com/in/dheo-apriansyah-b13100174/">LinkedIn</a>
+          <a target="_blank" href="https://github.com/DheoAp">Github</a>
+          <a target="_blank" href="https://www.youtube.com/channel/UCt8ZwXTNS9OaY3ioa8L13dA?view_as=subscriber">Youtube</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
