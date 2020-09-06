2d9f8d13ff0c1eecffbe7fa5b84ae117e618b560
DheoAp <dheoap21@gmail.com>
2020-09-06 07:44:08 +0700

    Menambahkan script js, pooper dan bootstrap

--- application/views/template/footer.php
+++ application/views/template/footer.php
@@ -2,5 +2,8 @@
     <p class="copyright">Copyright 2020. Dheo Apriansyah</p>
   </div>
 </div>
+<script type="text/javascript" src="<?= base_url().'assets/js/jquery-3.5.1.slim.min.js'?>"></script>
+<script type="text/javascript" src="<?= base_url().'assets/js/popper.min.js'?>"></script>
+<script type="text/javascript" src="<?= base_url().'assets/js/bootstrap.min.js'?>"></script>
 </body>
 </html>
